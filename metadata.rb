2dbf0baa54b9d44cf9f12a0c52a1a2de3bc69731
Miguel Ferreira <miguelferreira@me.com>
2015-10-12 13:20:24 +0200

    Bump to patched version 2.99.1001

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Mike Fiedler'
 maintainer_email  'miketheman@gmail.com'
 license           'Apache 2.0'
 description       'Installs and configures nginx'
-version           '2.99.0'
+version           '2.99.1001'
 
 suggests 'apt',      '~> 2.6.1'
 suggests 'yum',      '~> 3.5.2'
